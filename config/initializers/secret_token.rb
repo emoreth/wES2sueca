f9250a9f4adf8bfc97c6c47d73e24cea30a5e14f
Everton Moreth <everton.moreth@gmail.com>
2011-03-27 18:00:20 -0300

    Commit inicial da criação do projeto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WES2sueca::Application.config.secret_token = '82ccbc891357fb227ab79c031d47270324deff9a79d894a3987c47709df768089f209ceea66815d6f864fb1c778285eaf1ea9921b296285aa1bf7bea253c9600'
